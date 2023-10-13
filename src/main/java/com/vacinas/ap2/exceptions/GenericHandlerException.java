b45145ae3b73539c4d9628a941478b0aee212c6b
Ademario <ademarioteles@hotmail.com>
2023-10-13 20:13:54 -0300

    Refatorando Generic Exception

--- src/main/java/com/vacinas/ap2/exceptions/GenericHandlerException.java
+++ src/main/java/com/vacinas/ap2/exceptions/GenericHandlerException.java
@@ -3,26 +3,43 @@ package com.vacinas.ap2.exceptions;
 import com.vacinas.ap2.entity.Mensagem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class GenericHandlerException extends ResponseEntityExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(GenericHandlerException.class);
 
     @ExceptionHandler(CPFException.class)
-    public ResponseEntity handleException(CPFException e) {
+    protected ResponseEntity handleException(CPFException e) {
         Mensagem mensagem = new Mensagem("O CPF informado já encontra-se cadastrado em nossa base de dados!");
         LOGGER.info("Tratamentação de exceção CPFException: " + mensagem);
         return new ResponseEntity(mensagem, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(PacientNotFoundException.class)
-    public ResponseEntity handleException(PacientNotFoundException e) {
+    protected ResponseEntity handleException(PacientNotFoundException e) {
         Mensagem mensagem = new Mensagem("Paciente(s) não encontrado(s)");
         LOGGER.info("Tratamentação de exceção PacientNotFoundException: " + mensagem);
         return new ResponseEntity(mensagem, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Mensagem> message = new ArrayList<>();
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            message.add(new Mensagem(error.getDefaultMessage()));
+            LOGGER.info("Tratamento de Exceção MethodArgumentNotValidException: " + error.getDefaultMessage());
+        }
+        return handleExceptionInternal(ex, message, headers, HttpStatus.BAD_REQUEST, request);
     }
 }

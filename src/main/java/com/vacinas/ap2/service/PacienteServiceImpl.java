e4cfd0691a37c7478d3b157d61befcdda6972487
Victor Caetano <victor.caetano2008@hotmail.com>
2023-11-18 23:07:19 -0300

    correção de datas

--- src/main/java/com/vacinas/ap2/service/PacienteServiceImpl.java
+++ src/main/java/com/vacinas/ap2/service/PacienteServiceImpl.java
@@ -169,15 +169,15 @@ public class PacienteServiceImpl implements PacienteService {
     @Override
     public void inject() {
         Paciente pacient = new Paciente("6556b65c2ba8c674fd37b804","Pablo","Santos","12345678917",
-                "21-01-1995","Masculino","(74)99485365",new Endereco("av. 7 de setembro",65,"2 de julho","Salvador","BA"));
+                "1995-01-21","Masculino","(74)99485365",new Endereco("av. 7 de setembro",65,"2 de julho","Salvador","BA"));
         Paciente pacientUm = new Paciente("6556b65c2ba8c674fd37b803","Priscila","Fernandes","12345678914",
-                "21-07-1994","Feminino","(75)98485365",new Endereco("Av. luiz tarquinio",201,"Centro","Lauro de Freitas","BA"));
+                "1994-07-21","Feminino","(75)98485365",new Endereco("Av. luiz tarquinio",201,"Centro","Lauro de Freitas","BA"));
         Paciente pacientDois = new Paciente("6556b65c2ba8c674fd37b703","Henrique","Souto","12345674914",
-                "21-07-1994","Masculino","(81)98485465",new Endereco("Av. Olivia palito",74,"Centro","Caruaru","PE"));
+                "1994-07-21","Masculino","(81)98485465",new Endereco("Av. Olivia palito",74,"Centro","Caruaru","PE"));
         Paciente pacientTres = new Paciente("6556b65c2ba8c674fd37b712","Washinton","Flores","12345674935",
-                "21-07-1994","Masculino","(11)97485465",new Endereco("Av. Tiete",74,"São Paulo","São Paulo","SP"));
+                "1994-07-21","Masculino","(11)97485465",new Endereco("Av. Tiete",74,"São Paulo","São Paulo","SP"));
         Paciente pacientQuatro = new Paciente("6556b65c2ba8c674fd37b988","Felipe","Marques","15845672935",
-                "21-07-1992","Masculino","(21)97465469",new Endereco("Av. Brasil",23,"Rio de Janeiro","Rio de Janeiro","RJ"));
+                "1992-07-21","Masculino","(21)97465469",new Endereco("Av. Brasil",23,"Rio de Janeiro","Rio de Janeiro","RJ"));
         List<Paciente> pacientesInjectados = new ArrayList<>(Arrays.asList(pacient,pacientUm,pacientDois,pacientTres,pacientQuatro));
         pacienteRepository.saveAll(pacientesInjectados);
     }
